e48764de888635bfa9b7f8c64162b25c3abf11f9
Geoff Massanek <gmassanek@gmail.com>
2016-06-21 22:41:00 -0400

    Darker lines and an api

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -2,4 +2,12 @@ class TrailsController < ApplicationController
   def show
 
   end
+
+  def index
+    respond_to do |format|
+      format.html
+      format.json { render json: Trail.all }
+    end
+  end
+
 end
--- app/models/trail.rb
+++ app/models/trail.rb
@@ -3,7 +3,22 @@ class Trail
     Dir.entries(Rails.root.join('public/trails')).map do |file|
       next if file =~ /^\./
 
-      file.gsub(".gpx", "")
-    end.flatten.select(&:present?)
+      {
+        file: file.gsub(".gpx", ""),
+        lineStyle: {
+          color: colors[file[/.*\.(.*).gpx/,1]] || 'red',
+          opacity: '100%',
+          weight: 2,
+        }
+      }
+    end.compact
+  end
+
+  def self.colors
+    @colors ||= {
+      'gdm' => 'orange',
+      'ajm' => 'blue',
+      'gam' => 'yellow',
+    }
   end
 end
